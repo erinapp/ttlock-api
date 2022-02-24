e4ce3729b7c47c45176128b3ab750afe16af35a7
nald-dev <rynvva@gmail.com>
2022-02-25 06:56:12 +0700

    add api mapper

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { MapTTLockAPIs, APIConfigsType } from "./helpers/api-mapper"
+
+export const APIConfigs: APIConfigsType = {
+}  
+
+export type APIParamsType = {
+}
+
+const TTLockAPI = MapTTLockAPIs()
+
+export default TTLockAPI
