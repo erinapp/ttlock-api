27a5eccf8d37267c093c112a681e8e860960d675
nald-dev <rynvva@gmail.com>
2022-02-24 13:24:33 +0700

    add reset-password endpoint

---
+++ src/endpoints/reset-password/interfaces.ts
@@ -0,0 +1,12 @@
+export interface ParameterInterface {
+    clientId: string;
+    clientSecret: string;
+    username: string;
+    password: string;
+    date: string;
+}
+
+export interface ResponseInterface {
+    errcode: number;
+    errmsg: string;
+}
